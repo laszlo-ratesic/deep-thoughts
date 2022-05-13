01d05cff18239069418ef1041a748815a338652e
laszlo-ratesic <keenanrc@hotmail.com>
2022-05-13 13:05:07 -0500

    handle thought form

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,6 +3,7 @@ import { Navigate, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
@@ -76,6 +77,7 @@ const Profile = (props) => {
           />
         </div>
       </div>
+      <div className='mb-3'>{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
